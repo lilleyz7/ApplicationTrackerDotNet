bb425c171274155af820a872cb23236c88345f54
lilleyz7 <lilley5>
2025-05-15 23:43:56 -0700

    Manual Mapper

---
+++ Utils/ApplicationMapper.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationTracker.Dtos;
+using ApplicationTracker.Entities;
+
+namespace ApplicationTracker.Utils
+{
+    public class ApplicationMapper
+    {
+        public static Application Map(ApplicationDto applicationDto, CustomUser user)
+        {
+            return new Application
+            {
+                Company = applicationDto.Company,
+                Title = applicationDto.Title,
+                Notes = applicationDto.Notes,
+                Status = applicationDto.Status,
+                Link = applicationDto.Link,
+                UserId = user.Id,
+                CustomUser = user,
+            };
+        }
+    }
+}
